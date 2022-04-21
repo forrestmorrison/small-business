43edc2a238e31a6d9b2705d182d2ddc4f11ecdf6
forrestmorrison <forrestmorrison@gmail.com>
2022-04-21 12:22:50 -0500

    added nav & map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    this.initMap();
+  }
+
+  initMap = () => {
+    const rockstarpark = { lat: 29.95862, lng: -95.42057};
+    let map = new window.google.maps.Map(document.getElementById("map"), {
+      center: { lat: 39, lng: -96 },
+      zoom: 5,
+    });
+    const marker = new window.google.maps.Marker({
+      position: rockstarpark,
+      map: map,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h3>Austin Small Business</h3>
+          <nav>
+            <button>Listings</button>
+            <button>Login</button>
+          </nav>
+        </header>
+        <div id="map"></div>
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
